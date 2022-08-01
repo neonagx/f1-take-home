50a639f537737b17c07c248af3f164aebb9bf580
Sukhyun Baik <sukhyun.baik@leidos.com>
2022-08-01 14:37:53 -0400

    changes to overall

--- src/tests/Overall.test.tsx
+++ src/tests/Overall.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import Enzyme, { shallow } from 'enzyme';
 import Overall from '../components/Overall';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() });
 React.useLayoutEffect = React.useEffect;
@@ -11,9 +10,9 @@ describe('<Overall />', () => {
 
     test('should have Overall Standing Page', () => {
         const wrapper = shallow(<Overall />);
+        const overall = <h2>Overall Standing 2022</h2>;
 
         expect(wrapper.html()).toMatchSnapshot();
+        expect(wrapper.contains(overall)).toEqual(true);
     });
-
-
 })
